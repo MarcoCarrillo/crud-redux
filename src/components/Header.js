ab4e5e188a4fb2abac4a29e09c950bbd0aaabe0b
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-08-26 20:01:41 -0500

    Realizacion de routing y componentes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container">
-                <h1>CRUD - React, Redux, REST API y Axios</h1>
+                <h1>
+                    <Link 
+                        to={'/'}
+                        className="text-light"
+                    >
+                    CRUD - React, Redux, REST API y Axios
+                    </Link>
+                </h1>
             </div>
 
-            <a 
-                href="/productos/nuevo" 
+            <Link 
+                to={"/productos/nuevo"} 
                 className="btn btn-danger nuevo-post d-block d-md-inline-block"
-            >Agregar Producto &#43;</a>
+            >Agregar Producto &#43;</Link>
         </nav>
     );
 }
