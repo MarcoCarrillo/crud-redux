d055d70008d373550e6495c7191d39e4d61bec9c
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-08-29 23:27:01 -0500

    Agregando nuevo producto al state

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -6,7 +6,29 @@ import {
 
 //Crear nuevos productos
 export function crearNuevoProductoAction(producto){
-    return ()=>{
-        console.log(producto);
+    return (dispatch) => {
+        dispatch( agregarProducto() );
+
+        try {
+            dispatch( agregarProductoExito(producto) );
+        } catch (error) {
+            dispatch( agregarProductoError(true) );
+        }
     }
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true
+});
+
+//Si el producto se guarda en db 
+const agregarProductoExito = producto => ({
+    type:  AGREGAR_PRODUCTO_EXITO,
+    payload: producto
+});
+
+//Si hay error
+const agregarProductoError = () => ({
+
+})
