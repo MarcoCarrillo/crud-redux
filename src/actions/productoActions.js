5bdb4c4ec5b5400becc566fbb924052e73de07bc
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-09-01 16:53:28 -0500

    Obteniendo los productos en el state

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -66,10 +66,27 @@ const agregarProductoError = estado => ({
 export function obtenerProductosAction () {
     return async (dispatch) => {
         dispatch( descargarProductos() );
+
+        try {
+            const respuesta = await clienteAxios.get('productos');
+            dispatch( descargarProductosExitosa(respuesta.data) );
+        } catch (error) {
+            dispatch( descargarProductosError() );
+        }
     }
 }
 
 const descargarProductos = () => ({
     type: COMENZAR_DESCARGA_PRODUCTOS,
+    payload: true
+});
+
+const descargarProductosExitosa = productos =>({
+    type: DESCARGA_PRODUCTOS_EXITO,
+    payload: productos
+});
+
+const descargarProductosError = () => ({
+    type: DESCARGA_PRODUCTOS_ERROR,
     payload: true
 })
